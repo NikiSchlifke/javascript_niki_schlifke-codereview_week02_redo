f29712668473954dafa3eff2c2a7f0fe41cad69a
Nikolaus Schlifke <nikolaus.schlifke@kouba.at>
2017-02-28 18:30:39 +0100

    adding prompt for player names

--- js/game.js
+++ js/game.js
@@ -10,14 +10,28 @@ function threeDiceRoll(faces) {
     return [diceRoll(faces), diceRoll(faces), diceRoll(faces)];
 }
 
-function fillResults(results, container) {
+function fillRoll(results, container) {
     for (i = 0; i < container.length; i++) {
         container[i].innerHTML = results[i];
     }
 }
 
+function fillScore(score, container) {
+    container.innerHTML = score;
+}
+
 function playerRoll(dice_doms) {
     var results = threeDiceRoll(6);
-    fillResults(results, dice_doms);
-   	return results[0] + results[1] + results[2];
+    fillRoll(results, dice_doms);
+    return results[0] + results[1] + results[2];
 }
+
+function promptPlayer(name_dom, player_id) {
+    name_dom.textContent = prompt("Please enter Player" + player_id + "'s Name: ");
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    promptPlayer(document.getElementById('player1--name'), 1);
+    promptPlayer(document.getElementById('player2--name'), 2);
+});
+
